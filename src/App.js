90ef11befae487f13198a6fa5760d3430261b9fa
Abel <abelsena1@hotmail.com>
2020-03-04 17:18:39 -0300

    First Version of Project List Repositories of GitHub

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react'
+//import styled from 'styled-components'
+
+import GlobalStyle from './styles/global'
+
+import Main from './pages/Main/index'
+
+const App = () => (
+
+  <Fragment>
+   <GlobalStyle />
+    <Main />
+    </Fragment>
+)
+
+export default App
